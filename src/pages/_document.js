10c56432e55c90e7dc7a7efb6b0d3218e7cc6c85
Jair Anguiano <jairanpo@gmail.com>
2021-02-23 12:11:12 -0600

    Tailwind CDN

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,7 +15,13 @@ class MyDocument extends Document {
 
                     <link
                         href="https://fonts.googleapis.com/css2?family=Crimson+Text&family=Poppins&family=Source+Serif+Pro:weight@900&display=swap"
-                        rel="stylesheet"></link>
+                        rel="stylesheet" />
+                    <link
+                        rel="stylesheet"
+                        href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.3/tailwind.min.css"
+                        integrity="sha512-wl80ucxCRpLkfaCnbM88y4AxnutbGk327762eM9E/rRTvY/ZGAHWMZrYUq66VQBYMIYDFpDdJAOGSLyIPHZ2IQ=="
+                        crossOrigin="anonymous"
+                    />
                 </Head>
                 <body>
                 <Main/>
