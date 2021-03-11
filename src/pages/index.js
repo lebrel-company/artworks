bcb277384a7da1c96d2d3df3cd28da88826b822a
Jair Anguiano <jairanpo@gmail.com>
2021-03-11 15:59:12 -0600

    Cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,32 +21,16 @@ function Home() {
     return (
         <div className='w-screen h-screen'>
             <div className='h-full md:mx-10'>
-                <div
-                    className='
-                h-full
-                flex flex-col
-                '>
-                    <div className='
-                w-full
-                z-10 p-8 relative z-20'>
+                <div className='h-full flex flex-col'>
+                    <div className='w-full z-10 p-8 relative z-20'>
                         <Header/>
                     </div>
-                    <div className='
-                h-full
-                xl:w-1/2
-                relative z-20
-                mx-4
-                lg:mx-20
-                container-scrollbar overflow-y-scroll
-                '
-                         style={{direction: "rtl"}}
-                    >
+                    <div
+                        className='h-full xl:w-1/2 relative z-20 mx-4 lg:mx-20 container-scrollbar overflow-y-scroll'
+                        style={{direction: "rtl"}}>
                         <Section/>
                     </div>
-                    <div className='
-                lg:mx-20
-                xl:w-1/2 py-2 relative z-20'
-                    >
+                    <div className='lg:mx-20 xl:w-1/2 py-2 relative z-20'>
                         <Footer/>
                     </div>
                 </div>
