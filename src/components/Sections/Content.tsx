c4ca23c5f03d5ac7c20c77b0ba19c6da8f8cf14e
Jair Anguiano <jairanpo@gmail.com>
2022-11-10 20:52:52 -0600

    feat: add cyan/magenta text padding

--- src/components/Sections/Content.tsx
+++ src/components/Sections/Content.tsx
@@ -4,11 +4,13 @@ import styled from 'styled-components';
 import { colors } from '../../shared/theme'
 
 
+const textPadding = '.7rem'
 const Title = styled.div`
   color: ${colors.magenta};
   font-family: TimesNewRoman, serif;
   font-style: italic;
   font-size:1.8rem;
+  padding: 0rem 0rem ${textPadding} 0rem;
 `
 const BlueText = styled.div`
   color: ${colors.blue};
@@ -16,6 +18,7 @@ const BlueText = styled.div`
   text-family: Avenir, sans-serif;  
   letter-spacing: .5rem;
   font-size: 1.2rem;
+  padding: 0rem 0rem ${textPadding} 0rem;
 `
 const Parragraph = styled.div`
   text-family: Avenir, sans-serif;  
